8e1646b26254994d12365395d13951388b7e403f
nrzhang115 <159680727+nrzhang115@users.noreply.github.com>
2024-07-13 14:58:51 +1000

    Skip files that do not exist

--- prepare_datasets/prepare_shhs.py
+++ prepare_datasets/prepare_shhs.py
@@ -67,70 +67,81 @@ def main():
     for file_id in range(len(edf_fnames)):
         if os.path.exists(os.path.join(args.output_dir, edf_fnames[file_id].split('/')[-1])[:-4]+".npz"):
             continue
-        print(edf_fnames[file_id])
-
-        raw = read_raw_edf(edf_fnames[file_id], preload=True, stim_channel=None, verbose=None)
-        sampling_rate = raw.info['sfreq']
-        ch_type = args.select_ch.split(" ")[0]
-        select_ch = [s for s in raw.info["ch_names"] if ch_type in s][0]
-
-        raw_ch_df = raw.to_data_frame(scaling_time=sampling_rate)[select_ch]
-        raw_ch_df = raw_ch_df.to_frame()
-        raw_ch_df.set_index(np.arange(len(raw_ch_df)))
-
-        labels = np.ones(len(raw_ch_df))  # Initialize labels as one (for non-apnea events and apnea events are shorter than 10s)
         
-        # Read annotation and its header
-        t = ET.parse(ann_fnames[file_id])
-        r = t.getroot()
-        faulty_File = 0
-        for event in r.findall('.//ScoredEvent'):
-            event_name = event.find('Name').text
-            duration_check = float(event.find('Duration').text)
-            # Check the duration of apnea events
-            if event_name in apnea_events and duration_check >= 10:
-                # Convert time durations to sample indices
-                start_time = float(event.find('Start').text) * sampling_rate
-                duration = duration_check * sampling_rate
-                start_idx = int(start_time)
-                end_idx = int(start_time + duration)
-                labels[start_idx:end_idx] = 0  # Only marking apnea events with 0
+        # Check if annotation file exists
+        if not os.path.exists(ann_fnames[file_id]):
+            print(f"Annotation file not found: {ann_fnames[file_id]}, skipping corresponding EDF file: {edf_fnames[file_id]}")
+            continue
         
-        raw_ch = raw_ch_df.values
+        try: 
+            print(edf_fnames[file_id])
 
-        # Verify that we can split into 30-s epochs
-        if len(raw_ch) % (EPOCH_SEC_SIZE * sampling_rate) != 0:
-            raise Exception("Something wrong")
-        n_epochs = len(raw_ch) / (EPOCH_SEC_SIZE * sampling_rate)
+            raw = read_raw_edf(edf_fnames[file_id], preload=True, stim_channel=None, verbose=None)
+            sampling_rate = raw.info['sfreq']
+            ch_type = args.select_ch.split(" ")[0]
+            select_ch = [s for s in raw.info["ch_names"] if ch_type in s][0]
 
-        # Get epochs and their corresponding labels
-        x = np.asarray(np.split(raw_ch, n_epochs)).astype(np.float32)
-        y = np.asarray(np.split(labels, n_epochs)).astype(np.int32)
-        
-        assert len(x) == len(y)
+            # raw_ch_df = raw.to_data_frame(scaling_time=sampling_rate)[select_ch]
+            raw_ch_df = raw.to_data_frame(time_format=None)[select_ch] * (1.0 / sampling_rate)
+            raw_ch_df = raw_ch_df.to_frame()
+            raw_ch_df.set_index(np.arange(len(raw_ch_df)))
 
-        # Select on sleep periods
-        w_edge_mins = 30
-        nw_idx = np.where(y.sum(axis=1) != 0)[0]
-        start_idx = nw_idx[0] - (w_edge_mins * 2)
-        end_idx = nw_idx[-1] + (w_edge_mins * 2)
-        if start_idx < 0: start_idx = 0
-        if end_idx >= len(y): end_idx = len(y) - 1
-        select_idx = np.arange(start_idx, end_idx + 1)
-        print("Data before selection: {}, {}".format(x.shape, y.shape))
-        x = x[select_idx]
-        y = y[select_idx]
-        print("Data after selection: {}, {}".format(x.shape, y.shape))
+            labels = np.ones(len(raw_ch_df))  # Initialize labels as one (for non-apnea events and apnea events are shorter than 10s)
+            
+            # Read annotation and its header
+            t = ET.parse(ann_fnames[file_id])
+            r = t.getroot()
+            faulty_File = 0
+            for event in r.findall('.//ScoredEvent'):
+                event_name = event.find('Name').text
+                duration_check = float(event.find('Duration').text)
+                # Check the duration of apnea events
+                if event_name in apnea_events and duration_check >= 10:
+                    # Convert time durations to sample indices
+                    start_time = float(event.find('Start').text) * sampling_rate
+                    duration = duration_check * sampling_rate
+                    start_idx = int(start_time)
+                    end_idx = int(start_time + duration)
+                    labels[start_idx:end_idx] = 0  # Only marking apnea events with 0
+            
+            raw_ch = raw_ch_df.values
 
-        # Saving as numpy files
-        filename = os.path.basename(edf_fnames[file_id]).replace(".edf",  ".npz")
-        save_dict = {
-            "x": x,
-            "y": y,
-            "fs": sampling_rate
-        }
-        np.savez(os.path.join(args.output_dir, filename), **save_dict)
-        print(" ---------- Done this file ---------")
+            # Verify that we can split into 30-s epochs
+            if len(raw_ch) % (EPOCH_SEC_SIZE * sampling_rate) != 0:
+                raise Exception("Something wrong")
+            n_epochs = len(raw_ch) / (EPOCH_SEC_SIZE * sampling_rate)
+
+            # Get epochs and their corresponding labels
+            x = np.asarray(np.split(raw_ch, n_epochs)).astype(np.float32)
+            y = np.asarray(np.split(labels, n_epochs)).astype(np.int32)
+            
+            assert len(x) == len(y)
+
+            # Select on sleep periods
+            w_edge_mins = 30
+            nw_idx = np.where(y.sum(axis=1) != 0)[0]
+            start_idx = nw_idx[0] - (w_edge_mins * 2)
+            end_idx = nw_idx[-1] + (w_edge_mins * 2)
+            if start_idx < 0: start_idx = 0
+            if end_idx >= len(y): end_idx = len(y) - 1
+            select_idx = np.arange(start_idx, end_idx + 1)
+            print("Data before selection: {}, {}".format(x.shape, y.shape))
+            x = x[select_idx]
+            y = y[select_idx]
+            print("Data after selection: {}, {}".format(x.shape, y.shape))
+
+            # Saving as numpy files
+            filename = os.path.basename(edf_fnames[file_id]).replace(".edf",  ".npz")
+            save_dict = {
+                "x": x,
+                "y": y,
+                "fs": sampling_rate
+            }
+            np.savez(os.path.join(args.output_dir, filename), **save_dict)
+            print(" ---------- Done this file ---------")
+            
+        except Exception as e:
+            print(f"Error processing file {edf_fnames[file_id]}: {e}")
 
 if __name__ == "__main__":
     main()
